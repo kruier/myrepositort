1bebf2a7ee57c3fa866ac5a7213bd8174c3bd732
kruier <63445086+kruier@users.noreply.github.com>
2022-04-19 23:49:56 +0800

    Add files via upload

---
+++ 俄罗斯方块/Game/src/demo/Direction.java
@@ -0,0 +1,5 @@
+package demo;
+
+public enum Direction { //定义一个枚举方向，有四个取值分别是：向上、向下、向左、向右
+    top,bottom,left,right;
+}
---
+++ 俄罗斯方块/Game/src/demo/MySnake.java
@@ -0,0 +1,21 @@
+package demo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MySnake {
+    public static void main(String[] args) {
+        //创建一个窗口
+        JFrame frame = new JFrame();
+        //指定窗口x和y的位置以及窗口的宽度和高度值
+        frame.setBounds(600, 100, 700, 900);
+        //不允许拖拽改变大小
+        frame.setResizable(false);
+        //当点击窗口关闭按钮，执行操作是退出
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //添加画布
+        frame.add(new MyPanel());
+        //显示出来
+        frame.setVisible(true);
+    }
+}
